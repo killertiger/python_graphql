730b22fc76dc98d37c9450ec36171b2cc8bd3f0e
killertiger <moliveira@pinterest.com>
2024-10-16 03:32:22 -0700

    Add jobs and employers resolve

--- graphql_app/main.py
+++ graphql_app/main.py
@@ -23,12 +23,83 @@ class JobObject(ObjectType):
     employer = Field(lambda: EmployerObject)
 
 
+employers_data = [
+    {
+        "id": 1,
+        "name": "TechCorp Inc.",
+        "contact_email": "contact@techcorp.com",
+        "industry": "Technology",
+    },
+    {
+        "id": 2,
+        "name": "HealthWorks LLC",
+        "contact_email": "info@healthworks.com",
+        "industry": "Healthcare",
+    },
+    {
+        "id": 3,
+        "name": "EduSmart Solutions",
+        "contact_email": "support@edusmart.com",
+        "industry": "Education",
+    },
+    {
+        "id": 4,
+        "name": "Green Energy Co.",
+        "contact_email": "hello@greenenergy.com",
+        "industry": "Energy",
+    },
+    {
+        "id": 5,
+        "name": "Foodies Hub",
+        "contact_email": "careers@foodieshub.com",
+        "industry": "Food & Beverage",
+    },
+]
+
+jobs_data = [
+    {
+        "id": 1,
+        "title": "Software Engineer",
+        "description": "Responsible for developing and maintaining software solutions.",
+        "employer_id": 1,
+    },
+    {
+        "id": 2,
+        "title": "Project Manager",
+        "description": "Oversees projects from initiation to completion.",
+        "employer_id": 1,
+    },
+    {
+        "id": 3,
+        "title": "Registered Nurse",
+        "description": "Provides healthcare services and patient support.",
+        "employer_id": 2,
+    },
+    {
+        "id": 4,
+        "title": "Teaching Assistant",
+        "description": "Assists lead teachers in classroom activities and supervision.",
+        "employer_id": 3,
+    },
+    {
+        "id": 5,
+        "title": "Marketing Coordinator",
+        "description": "Supports marketing strategy with planning and execution.",
+        "employer_id": 4,
+    },
+]
+
+
 class Query(ObjectType):
-    hello = String(name=String(default_value="graphql"))
+    jobs = List(JobObject)
+    employers = List(EmployerObject)
 
     @staticmethod
-    def resolve_hello(root, info, name):
-        return f"Hello {name}"
+    def resolve_jobs(root, info):
+        return jobs_data
+    
+    def resolve_employers(root, info):
+        return employers_data
 
 
 schema = Schema(query=Query)
