36f0b9e6cbb8737f7c82003108c9be29d3d98bc2
killertiger <moliveira@pinterest.com>
2024-10-15 18:30:33 -0700

    Add playgrounds

--- graphql_app/README.md
+++ graphql_app/README.md
@@ -1,7 +1,14 @@
-
+# Recommended
+(Postman)[https://www.postman.com/] - Http request client tool
 
 # running
 
 ```bash
 uvicorn main:app --reload
 ```
+
+Access from your browser:
+
+Graphiql: http://localhost:8000/graphql/
+
+Playground: http://localhost:8000/graphql-p/
--- graphql_app/main.py
+++ graphql_app/main.py
@@ -1,6 +1,10 @@
 from graphene import Schema, ObjectType, String
 from fastapi import FastAPI
-from starlette_graphene3 import GraphQLApp
+from starlette_graphene3 import (
+    GraphQLApp,
+    make_graphiql_handler,
+    make_playground_handler,
+)
 
 
 class Query(ObjectType):
@@ -15,4 +19,6 @@ schema = Schema(query=Query)
 
 app = FastAPI()
 
-app.mount("/graphql", GraphQLApp(schema=schema))
+app.mount('/graphql-p', GraphQLApp(schema=schema, on_get=make_playground_handler()))
+
+app.mount("/graphql", GraphQLApp(schema=schema, on_get=make_graphiql_handler()))
