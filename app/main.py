512e36edaaf0b8fc7d8758a9c3b0ff99f8e4f927
killertiger <moliveira@pinterest.com>
2024-10-18 04:55:00 -0700

    Add route to query jobs and employers

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ from starlette_graphene3 import (
     make_playground_handler,
 )
 from app.db.database import prepare_database
+from app.db.models import Employer, Job
 from app.gql.queries import Query
+from app.db.database import Session
 
 schema = Schema(query=Query)
 
@@ -16,6 +18,18 @@ app = FastAPI()
 @app.on_event("startup")
 def startup_event():
     prepare_database()
+
+
+@app.get("/employers")
+def get_employers():
+    with Session() as session:
+        return session.query(Employer).all()
+
+
+@app.get("/jobs")
+def get_jobs():
+    with Session() as session:
+        return session.query(Job).all()
 
 
 app.mount('/graphql-p', GraphQLApp(schema=schema, on_get=make_playground_handler()))
