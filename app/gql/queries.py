cadd23a84714264d05d697368b27e5eb8f379559
killertiger <moliveira@pinterest.com>
2024-10-22 04:16:44 -0700

    Add Query filter by jobid

--- app/gql/queries.py
+++ app/gql/queries.py
@@ -1,20 +1,22 @@
-from graphene import List, ObjectType
+from graphene import Field, Int, List, ObjectType
 from app.db.models import Employer, Job
 from app.gql.types import JobObject, EmployerObject
 from app.db.database import Session
-from sqlalchemy.orm import joinedload
 
 
 class Query(ObjectType):
     jobs = List(JobObject)
+    job = Field(JobObject, id=Int(required=True))
     employers = List(EmployerObject)
 
     @staticmethod
     def resolve_jobs(root, info):
         return Session().query(Job).all()
-        # with Session() as session:
-        #     return session.query(Job).options(joinedload(Job.employer)).all()
 
     @staticmethod
     def resolve_employers(root, info):
         return Session().query(Employer).all()
+
+    @staticmethod
+    def resolve_job(root, info, id):
+        return Session().query(Job).filter(Job.id == id).first()
