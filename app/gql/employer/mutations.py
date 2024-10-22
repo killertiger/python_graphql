1fe9c423017867e6fa5f23efd997fd09c3871a47
killertiger <moliveira@pinterest.com>
2024-10-22 04:36:17 -0700

    Refactoring classes and folders

---
+++ app/gql/employer/mutations.py
@@ -0,0 +1,23 @@
+from graphene import Field, Mutation, String
+
+from app.db.models import Employer
+from app.gql.types import EmployerObject
+from app.db.database import Session
+
+
+class AddEmployer(Mutation):
+    class Arguments:
+        name = String(required=True)
+        contact_email = String(required=True)
+        industry = String(required=True)
+
+    employer = Field(lambda: EmployerObject)
+
+    @staticmethod
+    def mutate(root, info, name, contact_email, industry):
+        session = Session()
+        employer = Employer(name=name, contact_email=contact_email, industry=industry)
+        session.add(employer)
+        session.commit()
+        session.refresh(employer)
+        return AddEmployer(employer=employer)
--- app/gql/types.py
+++ app/gql/types.py
@@ -1,5 +1,4 @@
 from graphene import Field, Int, List, ObjectType, String
-from app.db.data import jobs_data, employers_data
 from app.db.models import Employer, Job
 
 
@@ -25,11 +24,4 @@ class JobObject(ObjectType):
     @staticmethod
     def resolve_employer(root: Job, info):
         return root.employer
-        # return next(
-        #     (
-        #         employer
-        #         for employer in employers_data
-        #         if employer["id"] == root["employer_id"]
-        #     ),
-        #     None,
-        # )
+   
