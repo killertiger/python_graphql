f9701c35aaaa14cee27ef309c8006f1c6560bf58
killertiger <moliveira@pinterest.com>
2024-10-27 07:04:07 -0700

    generate jwt token

--- app/gql/user/mutations.py
+++ app/gql/user/mutations.py
@@ -1,5 +1,6 @@
 import string
 from random import choices
+from datetime import datetime, timedelta
 from graphene import Mutation, String
 from graphql import GraphQLError
 from app.db.database import Session
@@ -7,7 +8,25 @@ from app.db.models import User
 
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
+
+from app.settings.config import ALGORITHM, SECRET_KEY, TOKEN_EXPIRATION_TIME_MINUTES
+
 ph = PasswordHasher()
+
+
+import jwt
+
+
+def generate_token(email):
+    expiration_time = datetime.utcnow() + timedelta(
+        minutes=TOKEN_EXPIRATION_TIME_MINUTES
+    )
+
+    payload = {"sub": email, "exp": expiration_time}
+
+    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+    return token
 
 
 class LoginUser(Mutation):
@@ -25,12 +44,13 @@ class LoginUser(Mutation):
 
         if not user:
             raise GraphQLError("A user by that email does not exist")
-        
+
         try:
             ph.verify(user.password_hash, password)
         except VerifyMismatchError:
             raise GraphQLError("Invalid password")
 
-        token = ''.join(choices(string.ascii_lowercase, k=10))
+        # token = ''.join(choices(string.ascii_lowercase, k=10))
+        token = generate_token(email)
 
         return LoginUser(token=token)
